0e1dcb8a15fd42842e4e83fdc94a2c05bc174caf
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-07-17 21:01:34 +0530

    Add files via upload

---
+++ Replace words/shell.sh
@@ -0,0 +1,19 @@
+ 
+
+sed -i -e 's/HEY/Hello/gI' mmk.txt
+
+
+
+#Hey Muthu
+#Replace the word "Hey"
+#New word "Hello" 
+# / act as Delimiter character
+# -i  to update file pass option
+# s/ substitute the found expression (Hey with Hello)
+# g/ means global replace
+# I to match replace multiple words in cases
+
+
+
+
+
